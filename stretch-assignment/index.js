3c988d95d4bd573bac74737e74fbd2dc5021ab60
David Horstman <davidzilla27@gmail.com>
2020-01-20 10:56:50 -0800

    Stretch: fixed. Thanks Sebastian Garces!

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -2,9 +2,9 @@ document.querySelector('.blocks').addEventListener('click', event =>
     event.target.parentNode.prepend(event.target)
 );
 document.querySelector('.blocks').addEventListener('mousedown', event => {
-    let style = event.target.style.transform || "translateX(0)";
-    let translate = parseInt(style.slice(style.indexOf("(") + 1, style.indexOf(")"))) + 1;
-    event.target.style.transform = "scaleX(" + translate + ")"; 
-    //event.target.style.transform = "translateX("+translate+")";
+    let style = event.target.style.transform || "translateX(0px)";
+    let translate = parseInt(style.slice(style.indexOf("(") + 1, style.indexOf("px)"))) + 10;
+    //event.target.style.transform = "scaleX(" + translate + ")"; 
+    event.target.style.transform = "translateX("+translate+"px)";
     console.log(event.target.style.transform);
 });
