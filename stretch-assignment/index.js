1b82fce4b8aeffe704d55e49be5a9e60b8cf18cd
David Horstman <davidzilla27@gmail.com>
2020-01-19 21:55:23 -0800

    Stretch: calling it a night.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -4,6 +4,7 @@ document.querySelector('.blocks').addEventListener('click', event =>
 document.querySelector('.blocks').addEventListener('mousedown', event => {
     let style = event.target.style.transform || "translateX(0)";
     let translate = parseInt(style.slice(style.indexOf("(") + 1, style.indexOf(")"))) + 1;
-    event.target.style.transform = 'scaleX(' + translate + ')'; //"translateX("+translate+")";
+    event.target.style.transform = "scaleX(" + translate + ")"; 
+    //event.target.style.transform = "translateX("+translate+")";
     console.log(event.target.style.transform);
 });
